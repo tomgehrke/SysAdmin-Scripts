2ec6eee5b543d681afae8f4924c2706e60bda59e
Tom Gehrke <tomgehrke@gmail.com>
2023-04-08 16:38:36 -0500

    Provide feedback on IPv6 disabling

--- Disable-IPv6.ps1
+++ Disable-IPv6.ps1
@@ -1,1 +1,3 @@
 Get-NetAdapterBinding -ComponentID ms_tcpip6 | Where-Object {$_.Enabled -eq $True} | ForEach-Object {Set-NetAdapterBinding -Name $_.Name -ComponentID $_.ComponentID -Enabled $False}
+Write-Output "Final Status..."
+Get-NetAdapterBinding -ComponentID ms_tcpip6
